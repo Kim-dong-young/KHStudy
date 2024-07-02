11c11b0e8cb57eaa3a5c4fe0645ec5919725ecc8
user1 <user1@DESKTOP-K5A03OM>
2024-07-02 17:49:37 +0900

    3일차

--- src/test01/TestCode3.java
+++ src/test01/TestCode3.java
@@ -1,12 +1,23 @@
 package test01;
 
+import java.util.Arrays;
+
 public class TestCode3 {
 	// https://school.programmers.co.kr/learn/courses/30/lessons/12939
-	// 작성중
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String s = "1 2 3 4";
+		String[] snum = s.split(" ");
+		int[] number = new int[snum.length];
+		
+		for(int i=0; i< number.length; i++) {
+			number[i]=Integer.parseInt(snum[i]);
+		}
+		
+		int max = Arrays.stream(number).max().getAsInt();
+		int min = Arrays.stream(number).min().getAsInt();
+		
+		System.out.println(Integer.toString(min) +" " +Integer.toString(max));
 	}
 
 }
---
+++ src/test01/TestCode4.java
@@ -0,0 +1,36 @@
+package test01;
+
+public class TestCode4 {
+	//https://school.programmers.co.kr/learn/courses/30/lessons/12909
+	//작성중
+	
+	public static void main(String[] args) {
+		String s = ")()(";
+		char[] stack = new char[s.length()];
+		int top = -1;
+		boolean answer = true;
+		
+		for(int i=0;i<stack.length;i++) {
+			stack[i] = ' ';
+		}
+		
+		for(int i=0;i<s.length();i++) {
+			if(s.charAt(i) == '(') {
+				top++;
+				stack[top] = '(';
+			}
+			else {
+				answer = (top >= 0) && (stack[top] != ' ') ? true : false;
+				if(answer == false) break;
+				
+				stack[i] = ' ';
+				top--;
+			}
+		}
+		
+		if(top != -1) answer = false;
+		
+		System.out.println(answer);
+	}
+
+}
